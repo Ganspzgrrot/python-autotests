8af10d1346db373e906222068dce685e1399c439
Vladislav Tiptsov <vladislav.tiptsov@ya.ru>
2026-05-04 18:28:46 +0300

    Настройка репозитория

--- conftest.py
+++ conftest.py
@@ -2,13 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def pytest_addoption(parser):
-    parser.addini("load_page_normal", "Normal page load")
-
 @pytest.fixture
-def driver(pytestconfig):
+def driver():
     options = Options()
-    options.page_load_strategy = pytestconfig.getini("load_page_normal")
+    options.page_load_strategy = 'normal'
     browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(30)
     yield browser
