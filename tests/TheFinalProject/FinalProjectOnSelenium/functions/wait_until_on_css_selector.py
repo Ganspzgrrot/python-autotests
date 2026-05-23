88191654a26af70e26c89a48f28e60ed9a20b327
Kerzherrdt <wlodziemerz2077@gmail.com>
2026-05-24 01:16:01 +0300

    Изменение файлов со вспомогательными функциями

--- tests/TheFinalProject/FinalProjectOnSelenium/functions/wait_until_on_css_selector.py
+++ tests/TheFinalProject/FinalProjectOnSelenium/functions/wait_until_on_css_selector.py
@@ -2,6 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-def wait_until(driver, locator, timeout=10):
+def wait_css(driver, locator, timeout=10):
     wait = WebDriverWait(driver, timeout)
     return wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, locator)))
--- tests/TheFinalProject/FinalProjectOnSelenium/functions/wait_until_on_id.py
+++ tests/TheFinalProject/FinalProjectOnSelenium/functions/wait_until_on_id.py
@@ -2,6 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-def wait_until(driver, locator, timeout=10):
+def wait_id(driver, locator, timeout=10):
     wait = WebDriverWait(driver, timeout)
     return wait.until(EC.presence_of_element_located((By.ID, locator)))
