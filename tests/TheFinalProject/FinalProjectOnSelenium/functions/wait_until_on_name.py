152873b46e75cddf8bbcd8afaf09f527688a583f
Kerzherrdt <wlodziemerz2077@gmail.com>
2026-05-23 23:50:49 +0300

    Создание вспомогательных функций с явным ожиданием и способом их поиска

---
+++ tests/TheFinalProject/FinalProjectOnSelenium/functions/wait_until_on_name.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+def wait_until(driver, locator, timeout=10):
+    wait = WebDriverWait(driver, timeout)
+    return wait.until(EC.presence_of_element_located((By.NAME, locator)))
